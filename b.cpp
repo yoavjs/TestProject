c240d43eedf7ad416495146b6d85608722477a5c
yoavjs <53790416+yoavjs@users.noreply.github.com>
2021-11-25 23:51:41 +0200

    Start
    first commit

---
+++ b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+typedef vector<vpl> vvpl;
+typedef vector<vpi> vvpi;
+
+int main(){
+
+    return 0;
+}
